e500cf929bd7462fb1d7305ae2a48477156ea5ab
EMMANUEL VILLADA SUAREZ <emmanuel.villada573@pascualbravo.edu.co>
2025-08-21 00:23:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { Toaster } from "@/components/ui/toast"
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: 'HomeBase Economy',
