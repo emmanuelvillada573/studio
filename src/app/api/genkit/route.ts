5334e1827466147bc1be5a60bca2c000b0bf3c01
EMMANUEL VILLADA SUAREZ <emmanuel.villada573@pascualbravo.edu.co>
2025-09-13 21:12:11 +0000

    force dynamic

--- src/app/api/genkit/route.ts
+++ src/app/api/genkit/route.ts
@@ -1,10 +1,13 @@
+// app/api/genkit/route.ts
+export const dynamic = 'force-dynamic';
+
 import { categorizeTransactionFlow } from '@/ai/flows/categorize-transaction';
-import  nextHandler  from '@genkit-ai/next';
+import nextHandler from '@genkit-ai/next';
 
 const handler = nextHandler(categorizeTransactionFlow) as unknown as {
-    GET: (req: Request) => Promise<Response>;
-    POST: (req: Request) => Promise<Response>;
-  };
-  
-  export const GET = handler.GET;
-  export const POST = handler.POST;
+  GET: (req: Request) => Promise<Response>;
+  POST: (req: Request) => Promise<Response>;
+};
+
+export const GET = handler.GET;
+export const POST = handler.POST;
