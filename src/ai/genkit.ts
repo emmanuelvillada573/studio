7f6ae06fbc6b182293fecac220d2cd7e9ac72e08
EMMANUEL VILLADA SUAREZ <emmanuel.villada573@pascualbravo.edu.co>
2025-09-13 21:06:57 +0000

    changing the genkit

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,19 +2,14 @@
 
 import {googleAI} from '@genkit-ai/googleai';
 import {genkit, type GenkitOptions} from 'genkit';
-import {firebase} from '@genkit-ai/firebase';
 
 const genkitOptions: GenkitOptions = {
   plugins: [
-    firebase(),
     googleAI({
       apiVersion: ['v1beta'],
     }),
   ],
-  logSinks: ['firebase'],
-  traceStore: 'firebase',
-  flowStateStore: 'firebase',
-  cacheStore: 'firebase',
+
 };
 
 export const ai = genkit(genkitOptions);
